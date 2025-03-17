e242a9112d1370e2131ce5873229c266cd6c7d89
Prabha Kumar <kprabhat628@gmail.com>
2025-03-17 20:59:10 +0530

    ok

--- src/app/lib/helpers/mail.js
+++ src/app/lib/helpers/mail.js
@@ -24,6 +24,17 @@ const sendMail = async ({ name, subject, email, message }) => {
       }
     });
 
+    // <header style="background-color: #043e96; color: white; padding: 10px 20px; display: flex; align-items: center; justify-content: center;">
+    //               <div style="position: relative; width: 120px; height: 40px;">
+    //                   <h1 id="heading" style="background-color: red; width: 100%; text-align: center; margin: 0; font-size: 20px; line-height: 40px; color: white;">
+    //                       Finnsap
+    //                   </h1>
+    //                   <img id="logo" src="${process.env.BASE_URL}/logo.png" alt="Finnsap Logo" 
+    //                       style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 100%; height: 100%;  border-radius: 6px;"
+    //                       >
+    //               </div>
+    //             </header>
+
     // 1st Email: Finnsap Team ko user ka message
     const mailOptions = {
       from: process.env.MAIL_USER,
@@ -32,19 +43,15 @@ const sendMail = async ({ name, subject, email, message }) => {
       html: `
         <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
           <div style="max-width: 600px; margin: auto; background: white; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 10px rgba(0,0,0,0.1);">
-                <header style="background-color: #043e96; color: white; padding: 10px 20px; display: flex; align-items: center; justify-content: center;">
-                  <div style="position: relative; width: 120px; height: 40px;">
-                      <h1 id="heading" style="background-color: red; width: 100%; text-align: center; margin: 0; font-size: 20px; line-height: 40px; color: white;">
-                          Finnsap
-                      </h1>
-                      <img id="logo" src="${process.env.BASE_URL}/logo.png" alt="Finnsap Logo" 
-                          style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 100%; height: 100%;  border-radius: 6px;"
-                          >
-                  </div>
-                </header>
-
-            
-
+                
+ <header
+                style="background-color: #043e96; color: white; padding: 10px 20px; display: flex; align-items: center; justify-content: center;">
+                <div
+                    style="position: relative; width: 120px; height: 40px; font-size: 25px; font-weight: 700; display: flex; justify-content: center; align-items: center;">
+                    Finnsap
+                    <img style="width: 100%; height: 100%; position: absolute; border-radius: 5px;" src="${process.env.BASE_URL}/logo.png" alt="Finnsap Logo">
+                </div>
+            </header>
 
 
             <div style="padding: 20px;">
@@ -77,16 +84,17 @@ const sendMail = async ({ name, subject, email, message }) => {
       html: `
             <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
               <div style="max-width: 600px; margin: auto; background: white; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 10px rgba(0,0,0,0.1);">
-                <header style="background-color: #043e96; color: white; padding: 10px 20px; display: flex; align-items: center; justify-content: center;">
-                  <div style="position: relative; width: 120px; height: 40px;">
-                      <h1 id="heading" style="background-color: red; width: 100%; text-align: center; margin: 0; font-size: 20px; line-height: 40px; color: white;">
-                          Finnsap
-                      </h1>
-                      <img id="logo" src="${process.env.BASE_URL}/logo.png" alt="Finnsap Logo" 
-                          style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 100%; height: 100%;  border-radius: 6px;"
-                          >
-                  </div>
-                </header>
+                
+
+                <header
+                style="background-color: #043e96; color: white; padding: 10px 20px; display: flex; align-items: center; justify-content: center;">
+                <div
+                    style="position: relative; width: 120px; height: 40px; font-size: 25px; font-weight: 700; display: flex; justify-content: center; align-items: center;">
+                    Finnsap
+                    <img style="width: 100%; height: 100%; position: absolute; border-radius: 5px;" src="${process.env.BASE_URL}/logo.png" alt="Finnsap Logo">
+                </div>
+            </header>
+
                 <div style="padding: 20px;">
                   <p style="font-size: 16px; color: #333;">Hello <strong>${name}</strong>,</p>
                   <p style="font-size: 16px; color: #333;">
